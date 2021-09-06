fea82eefa6596243c11d0fc68799ce27e8f57cba
Dariala <45213334+dariala@users.noreply.github.com>
2021-09-06 18:26:07 +0200

    Update InOrderTraversal.js

--- js/Graph/BinaryTree/DepthFirstTraversal/InOrderTraversal.js
+++ js/Graph/BinaryTree/DepthFirstTraversal/InOrderTraversal.js
@@ -1,12 +1,14 @@
-// Used to traverse values in tree from left to right!
+/** 
+    Used to traverse values in tree from left to right!
+*/
 
-function inOrderTraversalUsingRecursion(node, cb) {
+function inOrderTraversalUsingRecursion(node, callback) {
     if(node == null) {
         return;
     }
-    inOrderTraversalUsingRecursion(node.left, cb);
-    cb ? cb(node) : console.log(node.value);
-    inOrderTraversalUsingRecursion(node.right, cb);
+    inOrderTraversalUsingRecursion(node.left, callback);
+    callback ? callback(node) : console.log(node.value);
+    inOrderTraversalUsingRecursion(node.right, callback);
 }
 
 function toArray(tree) {
